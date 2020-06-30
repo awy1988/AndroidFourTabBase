e9a0a702f83569ecf340d50a12b03044e2a875ba
an.weiyang <>
2020-06-30 11:05:00 +0800

    Log收集逻辑更新： 1. 加入Log配置器 2. 加入可将Log缓存到文件的逻辑 3. 加入Log缓存到文件的路径可以自己指定的逻辑
    (cherry picked from commit db23120daf7b1e9c273499678db9e0da206d1490)
    (cherry picked from commit 026cd524d22b8407b68b2fe4548b43ff64bea4fc)

--- demo/corelib/src/main/java/com/demo/corelib/CoreLib.java
+++ demo/corelib/src/main/java/com/demo/corelib/CoreLib.java
@@ -1,16 +1,34 @@
 package com.demo.corelib;
 
 import android.app.Application;
+import com.demo.corelib.config.logger.LoggerConfig;
 import com.demo.corelib.util.Utils;
 
 public class CoreLib {
+
+    public static boolean IS_DEBUG;
+
+    private static LoggerConfig loggerConfig = new LoggerConfig();
 
     /**
      * 核心模块初始化方法
      * @param application
      */
-    public static void init(Application application) {
+    public static void init(Application application, boolean isDebug) {
+        IS_DEBUG = isDebug;
         Utils.init(application);
+        logInit();
+    }
+
+    /**
+     * log 输出配置
+     */
+    public static LoggerConfig loggerConfig() {
+        return loggerConfig;
+    }
+
+    private static void logInit() {
+        loggerConfig.init();
     }
 
 }
